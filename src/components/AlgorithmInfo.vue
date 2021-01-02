ae8de69e342c4ad97572149ac0af39297f6dbb96
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2021-01-02 01:26:20 +0000

    Added AlgorithmInfo

--- src/components/AlgorithmInfo.vue
+++ src/components/AlgorithmInfo.vue
@@ -1,9 +1,67 @@
 <template lang="pug">
-p Info
+.flex.flex-wrap.mt-6.mx-5.text-left
+  .flex.w-full.pb-5
+    h1.text-3xl Now showing: {{ $store.state.algorithm }}
+  .flex.justify-center.pb-5.w-full(class='lg:w-6/12')
+    table.table-auto.text-center
+      thead
+        tr
+          th(colspan=3) Time Complexity
+          th Space Complexity
+        tr 
+          th Best
+          th Average
+          th Worst
+          th Worst
+      tbody
+        tr(style='line-height: 2.5rem')
+          td(v-for='complexity in algorithmInfo.complexities')
+            code(:class='complexityColor(complexity)') {{ complexity }}
+  .w-full(class='lg:w-6/12')
+    p.mt-1 {{ algorithmInfo.text }}
+    p.mt-2
+      | Read more at:&nbsp
+      a.underline.text-blue-600.cursor-pointer(class='hover:text-blue-800 visited:text-purple-600"') {{ algorithmInfo.wikipedia }}
 </template>
 
 <script>
 export default {
-  name: 'AlgorithmInfo'
+  methods: {
+    complexityColor(complexity) {
+      console.log(typeof complexity);
+      return {
+        'px-3 py-1 rounded': true,
+        'bg-green-300': /.\(1\)/gm.test(complexity),
+        'bg-yellow-300': /.\(n\)/gm.test(complexity),
+        'bg-yellow-500': /.\(n log\(n\)\)/gm.test(complexity),
+        'bg-red-300': /.\(n\^2\)/gm.test(complexity)
+      };
+    }
+  },
+  computed: {
+    algorithmInfo() {
+      return this.$store.getters.currentInfo;
+    }
+  }
 };
 </script>
+
+<style scoped>
+td,
+tr {
+  padding: 0vw 3vw;
+}
+
+th {
+  border-radius: 5px;
+  padding: 3px;
+  border: 1px solid #ddd9ff;
+  background-image: linear-gradient(to bottom, #fff, #efedff);
+  font-weight: normal;
+}
+
+table {
+  border-collapse: separate;
+  border-spacing: 4px;
+}
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -18,6 +18,19 @@ const store = createStore({
       algorithms: ['Bubble Sort', 'Bogus Sort', 'Hamlalaa'],
       sizes: [4, 5, 6, 7, 8],
       speeds: ['Slow', 'Average', 'Fast']
+    },
+    algoInfo: {
+      'Bubble Sort': {
+        complexities: {
+          timeBest: 'Ω(n)',
+          timeAverage: 'Θ(n^2)',
+          timeWorst: 'O(n^2)',
+          spaceWorst: 'O(1)'
+        },
+        wikipedia: 'https://en.wikipedia.org/wiki/Bubble_sort',
+        text:
+          'Bubble Sort is a stable, in-place sorting algorithm that is named for the way smaller or larger elements "bubble" to the top of the list. \n Altough the algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort and is not recommended when n is large. The only significant advantage that bubble sort has over most other implementations, even quicksort, but not insertion sort, is the ability to detect if the list is already sorted.'
+      }
     }
   },
   mutations: {
@@ -36,7 +49,8 @@ const store = createStore({
     }
   },
   getters: {
-    slicedArray: state => state.numbers.slice(0, state.size)
+    slicedArray: state => state.numbers.slice(0, state.size),
+    currentInfo: state => state.algoInfo[state.algorithm]
   }
 });
 
