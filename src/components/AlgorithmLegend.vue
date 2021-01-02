d46980ff90775c6c722c91fc8d90f4e5a395f897
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2021-01-02 03:04:27 +0000

    Added AlgorithmLegend

--- src/components/AlgorithmLegend.vue
+++ src/components/AlgorithmLegend.vue
@@ -1,9 +1,25 @@
 <template lang="pug">
-p Legend
+.flex.justify-around.mb-10.max-w-2xl.mx-auto
+  .flex
+    .w-6.h-6.bg-red-200.rounded 
+    p.pl-2 Unsorted
+  .flex
+    .w-6.h-6.bg-green-200.rounded 
+    p.pl-1 Sorted
+  .flex
+    .w-6.h-6.bg-yellow-200.rounded 
+    p.pl-1 Comparing
+  .flex(v-if='isPivotAlgorithm')
+    .w-6.h-6.bg-purple-300.rounded 
+    p.pl-1 Pivot
 </template>
 
 <script>
 export default {
-  name: 'AlgorithmLegend'
+  computed: {
+    isPivotAlgorithm() {
+      return this.$store.state.algorithm == 'Quicksort';
+    }
+  }
 };
 </script>
