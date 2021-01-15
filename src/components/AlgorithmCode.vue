4fea42b99e0135d7bf4973039123439608cb682a
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2021-01-15 07:12:14 +0000

    Better highlited code and new basic explanation

--- src/components/AlgorithmCode.vue
+++ src/components/AlgorithmCode.vue
@@ -3,10 +3,10 @@
   .flex.flex-col.m-auto.text-left.w-auto(class='sm:mx-5 sm:w-full lg:w-auto lg:m-auto')
     .mb-2.p-1.rounded-md.shadow-lg.bg-light-pink
       p.ml-3.whitespace-pre {{ currStep.explanation }}
-    .px-2.py-2.bg-indigo-200.rounded-lg.shadow-lg(md:px-5)
+    .px-2.py-2.bg-indigo-200.rounded-lg.shadow-lg(class='md:px-5')
       pre(
         v-for='(codeBlock, index) in currAlgoData.code',
-        :class='[index == currStep.codeBlock ? "bg-indigo-400 text-white" : ""]'
+        :class='[index == currStep.codeBlock ? "bg-indigo-500 rounded opacity-80 text-white" : ""]'
       ) {{ codeBlock }}
 </template>
 
@@ -17,11 +17,11 @@ export default {
   computed: {
     currStep() {
       if (this.stepIndex == 0) {
-        return { codeBlock: 'block1', explanation: 'Set n to array length\nEnter do while loop' };
+        return { explanation: `Showing code for ${this.algorithm}\nPress play to go through the code` };
       }
       return this.steps[this.stepIndex - 1];
     },
-    ...mapState(['steps', 'stepIndex']),
+    ...mapState(['algorithm', 'steps', 'stepIndex']),
     ...mapGetters(['currAlgoData'])
   }
 };
