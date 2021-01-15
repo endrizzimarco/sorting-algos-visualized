30a4de4d7bd237338fc4d8c4d5f1ba3c9e16b971
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2021-01-15 05:06:29 +0000

    Tailwind apply directives moved to relevant files

--- src/components/AlgorithmLegend.vue
+++ src/components/AlgorithmLegend.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .flex.justify-around.max-w-2xl.mx-5(class='sm:mt-5 sm:mx-auto')
   .flex
-    .legend.border.border-gray-300
+    .legend.border.border-gray-400
     p.legendName Unsorted
   .flex
     .legend.bg-green-200
@@ -20,7 +20,7 @@ import { mapState } from 'vuex';
 export default {
   computed: {
     isRequired() {
-      return this.algorithm == 'Selection Sort';
+      return this.algorithm == 'Selection Sort' || this.algorithm == 'Insertion Sort';
     },
     legendName() {
       let name = this.algorithm == 'Quicksort' ? 'Pivot' : 'Current';
@@ -30,3 +30,12 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.legend {
+  @apply w-5 h-5 rounded sm:w-6 sm:h-6;
+}
+.legendName {
+  @apply pl-1 text-sm sm:text-base sm:pl-2;
+}
+</style>
